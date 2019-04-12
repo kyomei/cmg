5f602fdf56cc388830ddd751d262221093e15245
kyomei <rafa.jefer@gmail.com>
2019-04-12 10:35:48 -0300

    Painel: Navbar recriada com css puro

--- painel/index.php
+++ painel/index.php
@@ -1,69 +1,50 @@
-<?php
-$config['title'] = 'Página inicial';
-$config['navbar'] = '';
-?>
-<?php include_once '_includes/header.php'; ?> 
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Painel do Jogador - Crossfire Ghost Mode</title>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="">
+        <meta name="author" content="Rafael Jeferson">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- começa main content -->
-<article class="dashboard">
-    <div class="container">
-        <!-- First row -->
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card-box border-left-primary">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold">18</div>
-                            </div>
-                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card-box border-left-primary">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold">18</div>
-                            </div>
-                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card-box border-left-primary">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold">18</div>
-                            </div>
-                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card-box border-left-primary">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold">18</div>
-                            </div>
-                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</article>
+        <!-- Custom styles for this template -->
+        <link href="../assets/css/style_panel.css" rel="stylesheet">
 
-
-<?php include_once '_includes/footer.php'; ?>
+        <!-- fontawesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    </head>
+    <body>
+    	<!-- Start navbar -->
+	    	<nav id="menu">
+	    		<div class="container">
+		    		<!-- Brand -->
+		    		<div class="brand-nav">
+		    			<img src="https://cmg.ieatprofissionalizante.com.br/img/logo_cgm.png" width="40">
+		    		</div>
+		    		<ul>
+		    			<li><a href="#">Perfil</a></li>
+		    			<li><a href="#">Equipe</a></li>
+		    			<li><a href="#"><i class="fas fa-calendar-alt"></i></a></li>
+		    			<li><a href="#"><i class="fas fa-bell fa-fw"></i></a></li>
+		    			<li><a href="#"><i class="fas fa-envelope fa-fw"></i></a></li>
+		    			<li><a href="#">kyomei</a></li>
+		    		</ul>
+	    		</div>
+	    	</nav>
+    	<section id="dashboard" class="bg-light">
+    		<div class="container">   
+				<h1>Dashboard</h1>
+				<p>fasfsdafasd</p>
+    		</div>
+    	</section>
+    	<footer id="footer">
+    		<div class="container">
+    			<p>© 2019 Crossfire Ghost mode. Todos os direitos reservados. <a href="#">Termos de uso</a></p>
+    		</div>
+    	</footer>
+        <!-- script here -->
+        <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="../assets/js/script_panel.js"></script>
+    </body>
+</html>
---
+++ portal/index.php
@@ -0,0 +1,69 @@
+<?php
+$config['title'] = 'Página inicial';
+$config['navbar'] = '';
+?>
+<?php include_once '_includes/header.php'; ?> 
+
+<!-- começa main content -->
+<article class="dashboard">
+    <div class="container">
+        <!-- First row -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card-box border-left-primary">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold">18</div>
+                            </div>
+                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-box border-left-primary">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold">18</div>
+                            </div>
+                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-box border-left-primary">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold">18</div>
+                            </div>
+                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-box border-left-primary">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold">18</div>
+                            </div>
+                            <div class="col-auto"><i class="fas fa-comments fa-2x"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</article>
+
+
+<?php include_once '_includes/footer.php'; ?>
